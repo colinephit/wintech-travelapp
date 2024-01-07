e081d8a09599b28a200c98cb5ff8b57086e4f2ae
DESKTOP-SD7M0QE\user <peh.tingx@gmail.com>
2024-01-07 14:34:50 +0800

    search bar stores user input

--- src/components/SearchBarCity.js
+++ src/components/SearchBarCity.js
@@ -1,8 +1,23 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 export default function SearchBar() {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch("https://places.googleapis.com/v1/places:searchText")
+      .then((response) => response.json())
+      .then(json => {
+        console.log(json);
+      })
+  }
+
+  const handleChange = (value) => {
+    setInput(value)
+    fetchData(value)
+  }
+
   return (
     <Box
       component="form"
@@ -12,7 +27,7 @@ export default function SearchBar() {
       noValidate
       autoComplete="on"
     >
-      <TextField id="outlined-basic" label="Enter your destination city and favourite activities here." variant="outlined" />
+      <TextField id="outlined-basic" label="Enter your destination city and favourite activities here." variant="outlined" value={input} onChange={(e) => handleChange(e.target.value)}/>
     </Box>
   );
 }
